9f14ce08c209abbd24bb6f119cdcd591d6d63baf
Flye001 <philip.org1@gmail.com>
2024-01-01 20:44:11 +0000

    Created basic game object class

---
+++ FlyeEngine/GameObject.cs
@@ -0,0 +1,28 @@
+﻿using OpenTK.Mathematics;
+
+namespace FlyeEngine
+{
+    internal class GameObject
+    {
+        private Vector3 _position;
+        private Vector3 _rotation;
+        private Vector3 _scale;
+
+        private Mesh? _mesh;
+
+        public GameObject(Vector3 position, Vector3 rotation, Vector3 scale)
+        {
+            _position = position;
+            _rotation = rotation;
+            _scale = scale;
+        }
+
+        public GameObject(Vector3 position, Vector3 rotation, Vector3 scale, Mesh mesh)
+        {
+            _position = position;
+            _rotation = rotation;
+            _scale = scale;
+            _mesh = mesh;
+        }
+    }
+}
