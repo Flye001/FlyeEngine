5c62432031df350c042744471f8ac0bf5f4f0ea1
Flye001 <philip.org1@gmail.com>
2024-01-02 20:24:28 +0000

    Test with more objects

--- FlyeEngine/Transform.cs
+++ FlyeEngine/Transform.cs
@@ -4,8 +4,8 @@ namespace FlyeEngine
 {
     public class Transform
     {
-        public Vector3 Position { get; set; }
-        public Vector3 Rotation { get; set; }
-        public Vector3 Scale { get; set; }
+        public Vector3 Position { get; set; } = Vector3.Zero;
+        public Vector3 Rotation { get; set; } = Vector3.Zero;
+        public Vector3 Scale { get; set; } = Vector3.One;
     }
 }
--- TestGame/Program.cs
+++ TestGame/Program.cs
@@ -14,38 +14,43 @@ namespace TestGame
             game.LightPosition = new Vector3(0, 30f, 0f);
 
             // Add game objects
-            Transform objTrans = new()
-            {
-                Position = new Vector3(-5f, 0f, 15f),
-                Rotation = Vector3.Zero,
-                Scale = new Vector3(1f)
-            };
-            game.AddGameObjectWithMesh(objTrans, "MyObjects/cube.obj", ShaderTypeEnum.SingleColorWithLight, new Vector3(1f, 0.2f, 0.6f));
-            
-            Transform mountainTransform = new()
-            {
-                Position = new Vector3(10f, -15f, 30f),
-                Rotation = Vector3.Zero,
-                Scale = new Vector3(1f)
-            };
-            game.AddGameObjectWithMesh(mountainTransform, "MyObjects/mountains.obj", ShaderTypeEnum.SingleColorWithLight, new Vector3(0.7f, 0.4f, 0.1f));
+            //Transform objTrans = new()
+            //{
+            //    Position = new Vector3(-5f, 0f, 15f),
+            //    Rotation = Vector3.Zero,
+            //    Scale = new Vector3(1f)
+            //};
+            //game.AddGameObjectWithMesh(objTrans, "MyObjects/cube.obj", ShaderTypeEnum.SingleColorWithLight, new Vector3(1f, 0.2f, 0.6f));
+
+            //Transform mountainTransform = new()
+            //{
+            //    Position = new Vector3(10f, -15f, 30f),
+            //    Rotation = Vector3.Zero,
+            //    Scale = new Vector3(1f)
+            //};
+            //game.AddGameObjectWithMesh(mountainTransform, "MyObjects/mountains.obj", ShaderTypeEnum.SingleColorWithLight, new Vector3(0.7f, 0.4f, 0.1f));
 
             Transform planeTrans = new()
             {
-                Position = new Vector3(-5, 10, 30),
-                Rotation = new Vector3(-float.Pi / 6f, 0, float.Pi / 4f),
+                Position = new Vector3(-40, 20, -50),
+                Rotation = new Vector3(0, float.Pi / 2f, -float.Pi / 9f),
                 Scale = new Vector3(0.01f)
             };
-            Transform boring = new() { Position = Vector3.Zero, Rotation = Vector3.Zero, Scale = new Vector3(0.01f)};
+            Transform boring = new() { Position = Vector3.Zero, Rotation = Vector3.Zero, Scale = new Vector3(0.01f) };
             game.AddGameObjectWithTexture(planeTrans, "MyObjects/airplane.obj", "MyTextures/airplane.png", ShaderTypeEnum.Texture);
 
-            Transform lightT = new()
-            {
-                Position = new Vector3(0, 50f, 0f),
-                Rotation = Vector3.Zero,
-                Scale = Vector3.One
-            };
-            game.AddGameObjectWithMesh(lightT, "MyObjects/cube.obj", ShaderTypeEnum.SingleColor, new Vector3(1f, 1f, 0f));
+            //Transform lightT = new()
+            //{
+            //    Position = new Vector3(0, 50f, 0f),
+            //    Rotation = Vector3.Zero,
+            //    Scale = Vector3.One
+            //};
+            //game.AddGameObjectWithMesh(lightT, "MyObjects/cube.obj", ShaderTypeEnum.SingleColor, new Vector3(1f, 1f, 0f));
+
+            Transform spyroTransform = new();
+            game.AddGameObjectWithTexture(spyroTransform, "MyObjects/spyro.obj", "MyTextures/spyro.png", ShaderTypeEnum.Texture);
+            game.AddGameObjectWithMesh(spyroTransform, "MyObjects/3dcircle.obj", ShaderTypeEnum.SingleColorWithLight, new(1f, 0.3f, 0.2f));
+
 
             game.StartGame();
         }
